d9ff92082a442445eae24a6c3ee7b63174074559
Rafael Mamede <r.silvamamede@gmail.com>
2021-02-15 15:36:03 -0300

    readme.md update

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Star Wars Portal
 
-### project in development phase
+## How to launch the application
+### To inicialize this project
+run **npm i** inside the backend folder, and **npm i** inside frontend folder, to install all dependecies.
 
-To inicialize this project, run **npm i** inside the backend folder, and **npm i** inside frontend folder, to install all dependecies.
-After that, you can just start the backend and the frontend, running **npm start** inside backend folder and **ng serve** inside frontend folder. Besides, you need to have the mongodb, nodejs and the angular cli installed in your local machine.
+### To start the project
+You can start the backend and the frontend, running **npm start** inside *backend folder* and **ng serve** inside *frontend folder*. 
+
+### Dependencies
+Besides, you need to have the *Mongodb*, *Nodejs* and the *Angular CLI* installed in your local machine.
+
+## Project folders explication
+The application is divided in two folders.
+
+### *backend*, which is subdivided in:
+- api folder: It contains the files responsible to access the MongoDB database and inject the data received with some validations.
+- config folder: It contains the files responsible to configurate the parsers and the MongoDB connection.
+- index.js: File responsible to start the back-end server.
+
+### *frontend*, which is has the following structure:
+- src > components : All the created angular components that compose the front-end aplication.
+- src > components > character: The character components, responsibles to the CRUD interface.
+- src > components > template: All the page template components, as well as the header, content and footer.
+- src > components > views: Page components, that are showed in the main content component.
--- frontend/src/app/components/character/character.service.ts
+++ frontend/src/app/components/character/character.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { EMPTY, Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Character } from './character.model';
@@ -12,7 +13,7 @@ export class CharacterService {
 
   baseUrl: string = "http://localhost:3000/characters";
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient, private router: Router) { }
 
   showInfo(info: string): void {
     this.snackBar.open(info, 'fechar', {
@@ -40,7 +41,10 @@ export class CharacterService {
   create(character: Character): Observable<Character> {
     return this.http.post<Character>(this.baseUrl, character).pipe(
       map(obj => obj),
-      catchError(err => this.errorHandler(err.error))
+      catchError(err => {
+        if (err.status == 200) this.router.navigate(["/characters"]);
+        return this.errorHandler(err.error);
+      })
     );
   }
 
@@ -48,7 +52,10 @@ export class CharacterService {
     const url = `${this.baseUrl}/${id}`;
     return this.http.delete<Character>(url).pipe(
       map(obj => obj),
-      catchError(err => this.errorHandler(err.error))
+      catchError(err => {
+        if (err.status == 200) this.router.navigate(["/characters"]);
+        return this.errorHandler(err.error);
+      })
     );
   }
 
@@ -56,14 +63,17 @@ export class CharacterService {
     const url = `${this.baseUrl}/${character._id}`;
     return this.http.put<Character>(url, character).pipe(
       map(obj => obj),
-      catchError(err => this.errorHandler(err.error))
+      catchError(err => {
+        console.log(err)
+        if (err.status == 200) this.router.navigate(["/characters"]);
+        return this.errorHandler(err.error);
+      })
     )
   }
 
   errorHandler(err: any): Observable<any> {
     if (err.error) this.showInfo(err.text);
     else this.showInfo(err);
-    console.log(err)
     return EMPTY;
   }
 }
